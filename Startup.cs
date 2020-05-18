84ebe714be957b17168d39483e110673ee4a3f19
Chenting.Weng <wengct13@gmail.com>
2020-05-18 23:21:42 +0800

    1. 使用VS2019開啟，透過EF Core Power Tool進行DB First。 2. 新增appsettings.json的connection。 3. 在Setup.cs進行DbContext的DI。

--- Startup.cs
+++ Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HW01.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +27,11 @@ namespace HW01
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //DI
+            services.AddDbContext<ContosouniversityContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+
             services.AddControllers();
         }
 
